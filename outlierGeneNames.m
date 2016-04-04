3042e1fa7af0428b643eb280e8f06c12921f1f7f
Nimit <nimit.slash85@gmail.com>
2016-04-03 23:17:05 -0400

    Added file for outlier gene names

---
+++ outlierGeneNames.m
@@ -0,0 +1,20 @@
+% load outliers file
+someGenes = load('HH_BB1_gene_outliers.txt');
+
+% choose some number of genes
+K = 20;
+
+% sort first and second columns
+[col1, idx1] = sort(someGenes(:,1));
+[col2, idx2] = sort(someGenes(:,2));
+
+% get gene names
+C2 = B1H_0I_M0_R1_Jcel(idx2(1:K));
+C1 = B1H_0I_M0_R1_Jcel(idx1(1:K));
+
+% sum of the 2 columns
+col3 = someGenes(:,1) + someGenes(:,2);
+
+% sort the sum, get gene names
+[col3, idx3] = sort(col3);
+C3 = B1H_0I_M0_R1_Jcel(idx3(1:K));
